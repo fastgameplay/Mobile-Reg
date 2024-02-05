1f5a4ba03dba9c4e12c1e740fdc54bf7745754e9
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2024-02-06 01:03:28 +0400

    Basic Boolean Text

--- Assets/Scripts/UI/Text/BooleanTextManager.cs
+++ Assets/Scripts/UI/Text/BooleanTextManager.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BooleanTextManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace MobileReg.UI{
+    using UnityEngine;
+    using ScriptableEvents;
+    using MobileReg.Data;
+    using TMPro;
+    public class BooleanTextManager : MonoBehaviour
     {
-        
-    }
+        [Header("Events")]
+        [SerializeField] private SO_BaseEvent<bool> _onTextStateChange;
+        [SerializeField] private SO_BaseEvent _onTextCloseRequest;
+        [Space(10)]
+        [Header("Reference")] 
+        [SerializeField] private TextMeshProUGUI _currentText;
+        [SerializeField] private SO_BasicTextData _negativeText;
+        [SerializeField] private SO_BasicTextData _positiveText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTextStateChange(){
+
+        }
+        private void OnTextCloseRequest(){
+
+        }
+        private void OnEnable() {
+            _onTextCloseRequest += OnTextStateChange;
+            _onTextCloseRequest += OnTextCloseRequest;
+        }   
+        private void OnDisable() {
+            _onTextCloseRequest -= OnTextStateChange;
+            _onTextCloseRequest -= OnTextCloseRequest;
+
+        }    
+
     }
 }
